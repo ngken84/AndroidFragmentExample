9cbff32ddd6631cca66177ee1e81bc5c782386f3
Ken Ng <ng.ken84@gmail.com>
2018-09-10 00:22:25 -0700

    Set up Main Activity for Fragments

--- app/src/main/java/com/example/ngken/androidfragmentexample/MainActivity.java
+++ app/src/main/java/com/example/ngken/androidfragmentexample/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ngken.androidfragmentexample;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button homeBtn;
+    private Button profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeBtn = findViewById(R.id.main_home_btn);
+        profileBtn = findViewById(R.id.main_prof_btn);
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, fragment);
+        transaction.commit();
     }
 }
