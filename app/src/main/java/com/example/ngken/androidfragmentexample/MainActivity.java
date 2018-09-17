6caf8f019d3191b236f3ae936b3b07033002acfe
Ken Ng <ng.ken84@gmail.com>
2018-09-16 21:57:02 -0700

    Implement Display of Home and Profile Fragments

--- app/src/main/java/com/example/ngken/androidfragmentexample/MainActivity.java
+++ app/src/main/java/com/example/ngken/androidfragmentexample/MainActivity.java
@@ -1,29 +1,76 @@
 package com.example.ngken.androidfragmentexample;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener, HomeFragment.OnFragmentInteractionListener {
 
-    private Button homeBtn;
     private Button profileBtn;
+    private Button homeBtn;
 
+    private ProfileFragment profileFragment;
+    private HomeFragment homeFragment;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        homeBtn = findViewById(R.id.main_home_btn);
+        profileFragment = new ProfileFragment();
+        homeFragment = new HomeFragment();
+
+        showHomeFragment();
+
         profileBtn = findViewById(R.id.main_prof_btn);
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showProfileFragment();
+            }
+        });
+
+        homeBtn = findViewById(R.id.main_home_btn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHomeFragment();
+            }
+        });
 
     }
 
-    private void setFragment(Fragment fragment) {
+    private void showHomeFragment() {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.main_frame, fragment);
+        if(homeFragment.isAdded()) {
+            return;
+        }
+        transaction.remove(profileFragment);
+        transaction.add(R.id.main_linear, homeFragment);
         transaction.commit();
+    }
+
+    private void showProfileFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if(profileFragment.isAdded()) {
+            return;
+        }
+        transaction.remove(homeFragment);
+        transaction.add(R.id.main_linear, profileFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
